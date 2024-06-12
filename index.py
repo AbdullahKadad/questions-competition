d8a7ff926be99fb1e1226077524f1e5a2dcb98c3
AbdullahKadad <abdullahqdad1@gmail.com>
2024-06-12 00:58:34 +0000

    link to vercel 3

--- index.py
+++ index.py
@@ -5,9 +5,6 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-
         msg = "Welcome To Questions Competition"
         self.wfile.write(msg.encode())
-        
+        return

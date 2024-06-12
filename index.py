c4532343a934bd663637a8c4b084ba697847218c
AbdullahKadad <abdullahqdad1@gmail.com>
2024-06-12 01:02:10 +0000

    link to vercel 4

--- index.py
+++ index.py
@@ -3,8 +3,12 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-
         self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
         msg = "Welcome To Questions Competition"
         self.wfile.write(msg.encode())
         return
+
+# Export the handler as the main function
+main = handler
